ad338d6492486d685ff10dad90b44265efd3e02a
Sanskar31 <sanskarjain3112@gmail.com>
2025-06-26 20:33:00 +0530

    Add numerical stats

--- app.py
+++ app.py
@@ -1,23 +1,52 @@
 import streamlit as st
-import preprocessor
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import preprocessor, helper
 
 st.sidebar.title("Whatsapp Chat Analyzer")
 
-uploaded_file = st.sidebar.file_uploader("Choose a file")
+uploaded_file = st.sidebar.file_uploader("Choose a file", type=['txt'])
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
-    data = bytes_data.decode("utf-8")
+    data = bytes_data.decode("utf-8", errors="ignore")
     df = preprocessor.preprocess(data)
     st.dataframe(df)
 
     # fetch unique users
     user_list = df['user'].unique().tolist()
-    user_list.remove('group_notification')
+    if 'group_notification' in user_list:
+        user_list.remove('group_notification')
     user_list.sort()
-    user_list.insert(0,"Overall")
+    user_list.insert(0, "Overall")
 
-    selected_user = st.sidebar.selectbox("Show analysis wrt",user_list)
+    selected_user = st.sidebar.selectbox("Show analysis wrt", user_list)
 
+    if st.sidebar.button("Show Analysis"):
+        # Stats Area
+        num_messages, words, num_media_messages, num_links, emoji_count, chat_days = helper.fetch_stats(selected_user, df)
 
+        st.title("Top Statistics")
+        col1, col2, col3 = st.columns(3)
+        col4, col5, col6 = st.columns(3)
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+
+        with col2:
+            st.header("Total Words")
+            st.title(words)
+
+        with col3:
+            st.header("Media Shared")
+            st.title(num_media_messages)
+
+        with col4:
+            st.header("Links Shared")
+            st.title(num_links)
+
+        with col5:
+            st.header("Total Emojis")
+            st.title(emoji_count)
+
+        with col6:
+            st.header("Days Since Talking")
+            st.title(chat_days)
---
+++ helper.py
@@ -0,0 +1,38 @@
+from urlextract import URLExtract
+import emoji
+import pandas as pd
+
+extract = URLExtract()
+
+def fetch_stats(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    # Total messages
+    num_messages = df.shape[0]
+
+    # Total words
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    # Media messages
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
+
+    # Links
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+
+    # Emojis
+    emoji_count = 0
+    for message in df['message']:
+        emoji_count += len([char for char in message if emoji.is_emoji(char)])
+
+
+    # Chat duration (days)
+    start_date = df['date'].min().date()
+    end_date = df['date'].max().date()
+    duration_days = (end_date - start_date).days + 1  # +1 to include both ends
+
+    return num_messages, len(words), num_media_messages, len(links), emoji_count, duration_days
